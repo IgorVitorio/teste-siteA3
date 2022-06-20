afe1196ba712afd7e18db91377b21e7de1c4d8c9
Igor Vitório <105309839+IgorVitorio@users.noreply.github.com>
2022-06-20 15:51:46 -0300

    Update

--- script.js
+++ script.js
@@ -80,16 +80,9 @@ function init(resultFromServer){
     setPositionForWeatherInfo();
 }
 
-/* Tratar a posição do container que mostra as informações do tempo */
+/* Tratar a exibição do container que mostra as informações do tempo */
 function setPositionForWeatherInfo() {
     let weatherContainer = document.getElementById('weatherContainer');
-    let weatherContainerHeight = weatherContainer.clientHeight;
-    let weatherContainerWidth = weatherContainer.clientWidth;
-
-    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
-
-    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
-
     weatherContainer.style.visibility = 'visible';
 }
 
